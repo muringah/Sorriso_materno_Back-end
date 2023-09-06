bcaf67597e667588994075600afb13c018f3227a
unknown <mkayky60@gmail.com>
2023-09-06 12:52:15 -0300

    post feito

--- src/main/java/br/com/SorrisoMaterno/sorrisoapi/controller/ConsultaController.java
+++ src/main/java/br/com/SorrisoMaterno/sorrisoapi/controller/ConsultaController.java
@@ -1,14 +1,25 @@
 package br.com.SorrisoMaterno.sorrisoapi.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.SorrisoMaterno.sorrisoapi.model.Consulta;
-
+@RestController
 public class ConsultaController {
-    @PutMapping("/api/consulta/alterar/25/08/2023")
+    @PutMapping("/api/consulta/alterar")
     public Consulta endPoint1 (@RequestBody Consulta consultaParaAtualizar) {
         return consultaParaAtualizar;
+    }
+
+    @PostMapping("/api/consulta/agendamento")
+    public boolean criaConsulta(@RequestBody Consulta novaConsulta){
+        System.out.println("Nova consulta realizada com sucesso no dia:" + novaConsulta.getData());
+        if(novaConsulta.getData() != null){
+            return true;
+        }
+            return false;
     }
     
 }
--- src/main/java/br/com/SorrisoMaterno/sorrisoapi/controller/PacienteController.java
+++ src/main/java/br/com/SorrisoMaterno/sorrisoapi/controller/PacienteController.java
@@ -1,13 +1,11 @@
 package br.com.SorrisoMaterno.sorrisoapi.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import br.com.SorrisoMaterno.sorrisoapi.model.Paciente;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class PacienteController {
-    @PostMapping("/api/paciente/cadastro")
-    public Paciente novoCadastro(@RequestBody Paciente paciente) {
-        return paciente;
-    }
+  
+    
 }
